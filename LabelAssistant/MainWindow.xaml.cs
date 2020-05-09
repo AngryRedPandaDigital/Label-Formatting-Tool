e67d9d12afc0ef4f96e0aa43642ce52be565330f
chris <angryredpanda@gmail.com>
2020-05-09 18:43:26 -0500

    Add project files.

---
+++ LabelAssistant/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabelAssistant
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int whseTag;
+        List<LabelData> outputBuffer = new List<LabelData>();
+        string labelPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "locationlabels.txt");
+        string barcodePath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "locationbarcodes.txt");
+        //string waspPath;
+        public MainWindow()
+        {
+            InitializeComponent();
+            File.Create(labelPath);
+            File.Create(barcodePath);
+        }
+
+        private void mainSelect_Checked(object sender, RoutedEventArgs e)
+        {
+            whseTag = 20;
+        }
+
+        private void cageSelect_Checked(object sender, RoutedEventArgs e)
+        {
+            whseTag = 10;
+        }
+
+        private void addLabel_Click(object sender, RoutedEventArgs e)
+        {
+            LabelData temp = new LabelData();
+            temp.SetAisle(aisleNumber.Text);
+            temp.SetSection(sectionName.Text);
+            temp.SetShelf(shelfNumber.Text);
+            temp.SetLocation(positionNumber.Text);
+            temp.SetWhse(whseTag);
+            outputBuffer.Add(temp);
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void printButton_Click(object sender, RoutedEventArgs e)
+        {
+     
+                foreach (LabelData label in outputBuffer)
+                {
+                    File.AppendAllText(labelPath, label.GetLabelText() + Environment.NewLine);
+                File.AppendAllText(barcodePath, label.GetBarCode() + Environment.NewLine);
+                //Process.Start(waspPath);
+            }
+            }
+        
+    }
+}
---
+++ LabelData.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+public class Label
+{
+    private int whse = 20; //Variables for the label class.
+    private string aisle;
+    private string section;
+    private string shelf;
+    private string location;
+    private string barCode;
+    private string labelText;
+    public int creationErrorStatus;
+
+    public void BarCode()
+    {
+        barCode = Convert.ToString(whse) + aisle + section + Convert.ToString(shelf) + Convert.ToString(location);
+        //Derives the barcode string from previously input information.
+    }
+
+    public void Whse(int x)
+    {
+        whse = x;
+    }
+    public void LabelText()
+    {
+        labelText = aisle + "-" + section + "-" + shelf + "-" + location;
+        //Derives the Label Text from input information.
+    }
+    public string GetBarCode()
+    {
+        BarCode();
+        return (barCode);
+        //Creates barCode string and returns the value.
+    }
+    public void SetAisle(string value)
+    {
+        if (value.Length == 2)
+        {
+            aisle = value.ToUpper();
+            creationErrorStatus = 0;
+        }
+        else { creationErrorStatus = 1; }
+
+        //Handles errors and sets the value of aisle to user input.
+    }
+    public void SetSection(string value)
+    {
+        if (value.Length == 2)
+        {
+            section = value.ToUpper();
+            creationErrorStatus = 0;
+        }
+        else { creationErrorStatus = 1; }
+        //Handles errors and sets the value of section to user input.
+    }
+    public void SetLocation(string value)
+    {
+        if (value.Length == 2)
+        {
+            location = value;
+            creationErrorStatus = 0;
+        }
+        else { creationErrorStatus = 1; }
+        //Handles errors and sets the value of location to user input.
+    }
+    public void SetShelf(string value)
+    {
+        if (value.Length == 2)
+        {
+            shelf = value;
+            creationErrorStatus = 0;
+        }
+        else { creationErrorStatus = 1; }
+        //Handles errors and sets the value of shelf to user input.
+    }
+    public string GetLabelText()
+    {
+        LabelText();
+        return (labelText);
+        //Outputs the label text.
+    }
+}
